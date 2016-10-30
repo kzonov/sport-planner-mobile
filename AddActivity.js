b4906b9fc7f8205c97ba6a0b5acb784e644ffdb3
Kirill Zonov <graffzon@gmail.com>
2016-10-30 18:44:27 +0100

    Added new page, styled it and implemented dumb Async storage usage. Big commit

--- AddActivity.js
+++ AddActivity.js
@@ -7,6 +7,7 @@ import {
   TextInput,
   TouchableHighlight,
   ActivityIndicator,
+  AsyncStorage
 } from 'react-native';
 
 var styles = StyleSheet.create({
@@ -19,14 +20,100 @@ var styles = StyleSheet.create({
   container: {
     marginTop: 90,
     alignItems: 'center'
-  }});
+  },
+  standardInput: {
+    height: 36,
+    padding: 4,
+    marginRight: 5,
+    flex: 4,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#48BBEC',
+    borderRadius: 2,
+    color: '#48BBEC'
+  },
+  multilineInput: {
+    height: 72,
+    padding: 4,
+    marginRight: 5,
+    flex: 4,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#48BBEC',
+    borderRadius: 2,
+    color: '#48BBEC'
+  },
+  buttonText: {
+    marginTop: 1,
+    fontSize: 18,
+    color: 'black',
+    alignSelf: 'center'
+  },
+  button: {
+    height: 36,
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    borderRadius: 4,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  }
+});
 
 class AddActivity extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      date: new Date()
+      activityTitle: '',
+      activityDescription: ''
     };
+  }
+
+  async createActivity() {
+    activityData = {
+      date: this.props.date,
+      activityTitle: this.state.activityTitle,
+      activityDescription: this.state.activityDescription
+    }
+
+    try {
+
+      AsyncStorage.setItem('UID123', JSON.stringify([{a: 1}]), () => {
+        AsyncStorage.mergeItem('UID123', JSON.stringify([activityData]), () => {
+          AsyncStorage.getItem('UID123', (err, result) => {
+            console.log(result);
+          });
+        });
+      });
+
+      // var activities = await AsyncStorage.getItem('@ActivityStore:activities');
+      // console.log('old data: ' + activities);
+      // var array = await AsyncStorage.mergeItem('@ActivityStore:activities', JSON.stringify(activityData));
+      // console.log('new data: ' + array);
+      // await AsyncStorage.getItem('@ActivityStore:activities', (err, result) => {
+      // });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async getAndUseVariables() {
+    var activities = await AsyncStorage.getItem('@ActivityStore:activities');
+    this.setState({ activities: activities });
+  }
+
+
+  async onAddPressed() {
+    try {
+      await this.createActivity();
+      await this.getAndUseVariables();
+    } catch (error) {
+      // console.error('failed to save the data :(')
+      console.error(error);
+    }
   }
 
   render() {
@@ -34,7 +121,25 @@ class AddActivity extends Component {
       <View>
         <View style={styles.container}>
           <Text style={styles.description}>
-            {'Add an activity for the ' + this.state.date}
+            {'Add an activity for the ' + this.props.date}
+          </Text>
+          <TextInput
+            style={styles.standardInput}
+            value={this.state.activityTitle}
+            onChangeText={(title) => {this.setState({ activityTitle: title })}}
+            placeholder='Title of the activity'/>
+          <TextInput
+            style={styles.multilineInput}
+            value={this.state.activityDescription}
+            onChangeText={(description) => {this.setState({ activityDescription: description })}}
+            multiline={true}
+            placeholder='Description for the activity'/>
+          <TouchableHighlight style={styles.button} onPress={this.onAddPressed.bind(this)}
+              underlayColor='#99d9f4'>
+            <Text style={styles.buttonText}>Add</Text>
+          </TouchableHighlight>
+          <Text style={styles.description}>
+            '{this.state.activities}'
           </Text>
         </View>
       </View>
